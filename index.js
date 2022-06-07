4c2f2b4564e3f466e1d2aeaa1ba5a41e87cd1e6c
TingJun311 <swengineerwithisaac@gmail.com>
2022-06-07 23:57:23 +0800

    Updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+
 const filePaths = [
+    // To store all the sounds file path
     "./sounds/tom-1.mp3",
     "./sounds/tom-2.mp3",
     "./sounds/tom-3.mp3",
@@ -9,21 +11,41 @@ const filePaths = [
 ];
 
 document.querySelectorAll(".drum").forEach((e) => {
+    // For each HTML elements add a listener
     e.addEventListener("click", () => {
+        // Get the attribute string then wait for callback functions
         soundsDecider("w drum", "a drum", "s drum", "d drum", "j drum", "k drum", "l drum", e.getAttribute("class"));
+
+        // Get a character in the class name
+        animation(e.getAttribute("class").substring(0, 1));
     });
 });
 
 document.addEventListener("keypress", (e) => {
+    // If user click keyboard instead
+    // Get e object properties of key
     soundsDecider("w", "a", "s", "d", "j", "k", "l", e.key);
+    animation(e.key);
 });
 
+const animation = (e) => {
+    // This function its to set animation depending on what user have click or enter
+    const event = document.querySelector(`.${e}`);
+    event.classList.add("pressed")
+    setTimeout(() => {
+        event.classList.remove("pressed")
+    }, 100);
+    // 0.100 sec
+};
+
 function triggerSounds(path) {
+    // This function allows user to trigger the sound from the file path
     let audio = new Audio(path);
     audio.play();
 }
 
 function soundsDecider(key1, key2, key3, key4, key5, key6, key7, event) {
+    // This function allows the program decide which sounds to trigger 
     switch (event) {
         case key1:
             triggerSounds(filePaths[6]);
