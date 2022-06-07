7cb5ee39f349d44ad2a34996216b109606490d82
TingJun311 <swengineerwithisaac@gmail.com>
2022-06-07 23:09:11 +0800

    Updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const filePaths = [
     "./sounds/tom-1.mp3",
     "./sounds/tom-2.mp3",
@@ -6,41 +5,48 @@ const filePaths = [
     "./sounds/tom-4.mp3",
     "./sounds/snare.mp3",
     "./sounds/kick-bass.mp3",
-    "./sounds/crash.mp3"
-]
+    "./sounds/crash.mp3",
+];
 
 document.querySelectorAll(".drum").forEach((e) => {
     e.addEventListener("click", () => {
-        switch (e.getAttribute("class")) {
-            case "w drum":
-                triggerSounds(filePaths[6]);
-                break;
-            case "a drum":
-                triggerSounds(filePaths[5]);    
-                break;
-            case "s drum":
-                triggerSounds(filePaths[4]);
-                break;
-            case "d drum":
-                triggerSounds(filePaths[3]);
-                break;
-            case "j drum":
-                triggerSounds(filePaths[2]);
-                break;
-            case "k drum":
-                triggerSounds(filePaths[1]);
-                break;
-            case "l drum":
-                triggerSounds(filePaths[0]);
-                break;        
-            default:
-                console.log("class name error");    
-        }
-        console.log(e.getAttribute("class"));
+        soundsDecider("w drum", "a drum", "s drum", "d drum", "j drum", "k drum", "l drum", e.getAttribute("class"));
     });
 });
 
-function triggerSounds (path) {
+document.addEventListener("keypress", (e) => {
+    soundsDecider("w", "a", "s", "d", "j", "k", "l", e.key);
+});
+
+function triggerSounds(path) {
     let audio = new Audio(path);
     audio.play();
+}
+
+function soundsDecider(key1, key2, key3, key4, key5, key6, key7, event) {
+    switch (event) {
+        case key1:
+            triggerSounds(filePaths[6]);
+            break;
+        case key2:
+            triggerSounds(filePaths[5]);
+            break;
+        case key3:
+            triggerSounds(filePaths[4]);
+            break;
+        case key4:
+            triggerSounds(filePaths[3]);
+            break;
+        case key5:
+            triggerSounds(filePaths[2]);
+            break;
+        case key6:
+            triggerSounds(filePaths[1]);
+            break;
+        case key7:
+            triggerSounds(filePaths[0]);
+            break;
+        default:
+            console.log(event);
+    }
 }
